a7c69f980230f447cac987704a8543dc860200da
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-01 23:51:06 +0530

    expense terraform dev

--- 05-apps/expense.sh
+++ 05-apps/expense.sh
@@ -3,12 +3,7 @@
 # By default, user data will get sudo access.
 dnf install ansible -y
 cd /tmp
-# git clone https://github.com/Lingaiahthammisetti/4.10.1.expense-ansible-roles-tf-terraform-dev.git
-# cd 4.10.1.expense-ansible-roles-tf-terraform-dev
-git clone https://github.com/Lingaiahthammisetti/4.12.expense-ansible-roles-tf-terraform-dev.git
-cd 4.12.expense-ansible-roles-tf-terraform-dev
+git clone https://github.com/Lingaiahthammisetti/4.12.expense-ansible-roles-expense-terraform-dev.git
+cd 4.12.expense-ansible-roles-expense-terraform-dev
 ansible-playbook main.yaml -e component=backend -e login_password=ExpenseApp1
 ansible-playbook main.yaml -e component=frontend
-
-
-
